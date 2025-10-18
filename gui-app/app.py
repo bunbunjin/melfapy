d6202306203befdfc22a826483d14a5a0360e6a9
bunbunjin <bunbunjin.aisu@gmail.com>
2025-10-18 14:37:22 +0900

    Add text input for angle

--- gui-app/app.py
+++ gui-app/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from melfapy.Melfa import *
-
+import math
 
 st.title("Melfapy App")
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     x = st.text_input("x")
@@ -11,11 +11,16 @@ with col2:
     y = st.text_input("y")
 with col3:
     z = st.text_input("z")
+with col4:
+    angle = st.text_input("angle")
+    math.radians(float(angle))
 
 st.write("x:", x, "y:", y, "z:", z)
+
 btn = col1.button("Send Coodinates")
-pose = [x, y, z, 0, 0, 0, 0, 0, 4, 0]
+pose = [x, y, z, 0, 0, angle, 0, 0, 4, 0]
 datalink = MelfaDatalink(pose)
+
 print(datalink)
 if btn:
     st.write("Send Coodinates")
